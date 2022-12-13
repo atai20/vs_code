8167df58fbf01d0e814aa4c4b67d1b62c73e929e
atai20 <ataikyd2005@gmail.com>
2022-12-12 21:59:14 -0500

    new thing

---
+++ cpp_projects/PhysObj.hpp
@@ -0,0 +1,11 @@
+#include <string>
+
+class PhysObj {
+    double x;
+    double y;
+    int time;
+    double mass;
+    public:
+        PhysObj(double new_x, double new_y, int new_time, double new_mass);
+        
+};
---
+++ cpp_projects/program_phys.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "PhysObj.hpp"
+
+int main(){
+    PhysObj ball(1.0,1.0,1.0,1.0);
+    std::cout<<12;
+}
