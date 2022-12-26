f9aa924931902a6999ed9e0223496db067c305fd
atai20 <ataikyd2005@gmail.com>
2022-12-25 23:24:00 -0500

    new force

---
+++ cpp_projects/all_forces.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "PhysObj.hpp"
+#include <cmath>
+
+int all_forces(PhysObj new_obj){
+    PhysObj::PhysObj gravity(const G = 9.8){
+        new_obj.force(M_PI, G);
+        return new_obj;
+    }
+}
--- cpp_projects/my_program.cpp
+++ cpp_projects/my_program.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "PhysObj.hpp"
+#include "all_forces.hpp"
 #include <cmath>
 
 int main(){
     PhysObj atest(1, 2, 3, 4);
     std::cout<<"x = "<<atest.x<<", y = "<<atest.y<<"; ";
-    atest.force(M_PI, 1);
+    atest.force(M_PI/2, 1);
     std::cout<<"x = "<<atest.x<<", y = "<<atest.y;
+    atest = all_forces(atest).gravity(12);
+    std::cout<<"x = "<<atest.x<<", y = "<<atest.y;
+    
 }
